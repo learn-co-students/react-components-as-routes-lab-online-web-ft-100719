99067e98f1838184c1c6f30c08bec5819bc41e1b
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-02 11:36:31 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { actors } from '../data';
 
+let actorsInfo = []
+
+const renderActors = () => {
+  actorsInfo =  actors.map((actor, i) => {
+    return <div key={i}>
+      <h2>Name: {actor.name}</h2>
+      <h4>Movies:</h4>
+      <ul>
+        {actor.movies.map((movie, i) => {
+          return <li key={i}>
+            {movie}
+          </li>
+        })}
+      </ul>
+    </div>
+  })
+  return actorsInfo
+}
+
+renderActors()
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsInfo}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { directors } from '../data';
 
+let directorsInfo = []
+
+const renderDirectors = () => {
+  directorsInfo = directors.map((director, i) => {
+    return <div key={i}>
+      <h2>Name: {director.name}</h2>
+      <h4>Movies:</h4>
+      <ul>
+        {director.movies.map((movie, i) => {
+          return <li key={i}>
+            {movie}
+          </li>
+        })}
+      </ul>
+    </div>
+  })
+  return directorsInfo
+}
+
+renderDirectors()
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsInfo}
     </div>
   );
 }
